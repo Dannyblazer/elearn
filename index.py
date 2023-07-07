0c75e801405079880cb16a7209c83245e3de7351
Dannyblazer <nnajidanny004@gmail.com>
2023-07-07 21:02:52 +0100

    main project setup

--- index.py
+++ index.py
@@ -1,15 +1,16 @@
+import os
 import openai
-from extvar import *
-from llama_index import SimpleDirectoryReader, QuestionAnswerPrompt, StorageContext, load_index_from_storage
-from llama_index.storage.index_store import SimpleIndexStore
-openai.api_key= OPENAI_API_KEY
+from dotenv import load_dotenv
+from llama_index import StorageContext, SimpleDirectoryReader, load_index_from_storage
 
-storage_context = StorageContext.from_defaults(
-  
-    index_store=SimpleIndexStore.from_persist_dir(persist_dir="index"),
-)
+load_dotenv()
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+documents = SimpleDirectoryReader('data').load_data()
+
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
+# load index
 index = load_index_from_storage(storage_context)
-print('yay!')
 query_engine = index.as_query_engine()
-response1 = query_engine.query("What's the candidate's name?")
-print(response1)
+address = query_engine.query("Where is the candidate's address and Name?")
+print(address)
