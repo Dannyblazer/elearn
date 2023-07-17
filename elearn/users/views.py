f6b5ac8388c18ce0a7a954c47c2968a002279057
Dannyblazer <nnajidanny004@gmail.com>
2023-07-17 20:43:28 +0100

    user views and template

--- elearn/users/views.py
+++ elearn/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .forms import CustomUserCreation
+from django.contrib.auth import login as dj_login, authenticate, logout
 # Create your views here.
+
+def registration_view(request):
+    context = []
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = CustomUserCreation(request.POST)
+            if form.is_valid():
+                form.save()
+                
