8f0d1439efc05a1165b9daeb4717ca783f874131
Dannyblazer <nnajidanny004@gmail.com>
2023-08-12 19:06:14 +0100

    added a tst html

--- elearn/users/views.py
+++ elearn/users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from .forms import CustomUserCreation
 from django.contrib.auth import login as dj_login, authenticate, logout
 # Create your views here.
@@ -10,4 +12,17 @@ def registration_view(request):
             form = CustomUserCreation(request.POST)
             if form.is_valid():
                 form.save()
-                
+                email = form.cleaned_data('email')
+                raw_password = form.cleaned_data('password')
+                account = authenticate(email=email, password=raw_password)
+                dj_login(request, account)
+                return HttpResponseRedirect(reverse('home'))
+            else:
+                context['registration_form'] = form
+        else:
+            form = CustomUserCreation
+            context['registration_form'] = form
+        return render(request=request, template_name='users/register.html', context={'registration_form':form})
+    else:
+        return redirect('home')
+    
